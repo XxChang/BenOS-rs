e4c9d1fb2bd801723ae6d1fbf4a9de68bd40c50e
XxChang <xuxchang@hotmail.com>
2026-06-02 23:06:01 +0800

    Add VersatilePB PL110 LCD console

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+#[cfg(target_board = "versatilepb")]
+mod lcd;
 mod uart;
 
 use core::arch::global_asm;
@@ -11,13 +13,24 @@ pub extern "C" fn kernel_main() {
     let uart = unsafe { uart::Uart::new() };
     uart.init();
     uart.puts("Welcome BenOS!\n");
+
+    #[cfg(target_board = "versatilepb")]
+    {
+        let lcd = unsafe { lcd::Lcd::new() };
+        lcd.init();
+        lcd::init_printing(lcd, lcd::Rgb565::WHITE, lcd::Rgb565::BLACK);
+        lcd_println!("BenOS PL110 LCD");
+    }
+
     loop {
         let c = uart.getc();
         uart.putc(c);
     }
 }
 
-global_asm!("
+#[cfg(target_board = "raspi4b")]
+global_asm!(
+    "
     .section \".text.boot\"
 
     .global _start, memzero
@@ -27,10 +40,7 @@ global_asm!("
         and x0, x0, #0xff
         cbz x0, master
         b proc_hang
-    
-    proc_hang:
-        b proc_hang
-    
+
     master:
         adr x0, bss_begin
         adr x1, bss_end
@@ -40,10 +50,54 @@ global_asm!("
         mov sp, x0
         bl kernel_main
         b proc_hang
+    
 
     memzero:
         str xzr, [x0], #8
         subs x1, x1, #8
         b.gt memzero
         ret
-");
+"
+);
+
+#[cfg(target_board = "versatilepb")]
+global_asm!(
+    "
+    .section \".text.boot\"
+
+    .global _start
+    _start:
+        // VersatilePB只有一个处理器，直接跳到master
+        b master
+
+    master:
+        ldr r0, =bss_begin
+        ldr r1, =bss_end
+        sub r1, r1, r0
+        bl memzero
+        ldr r0, =_stack_top
+        mov sp, r0
+        bl kernel_main
+        b proc_hang
+
+
+    memzero:
+        cmp r1, #0
+        beq memzero_done
+        mov r2, #0
+    memzero_loop:
+        str r2, [r0], #4
+        subs r1, r1, #4
+        bgt memzero_loop
+    memzero_done:
+        bx lr
+"
+);
+
+global_asm!(
+    "
+    .global memzero, proc_hang
+    proc_hang:
+        b proc_hang
+"
+);
--- src/uart.rs
+++ src/uart.rs
@@ -5,7 +5,10 @@ use tock_registers::{
 };
 
 // PL011 UART0 base address (QEMU raspi4b / BCM2711)
+#[cfg(target_board = "raspi4b")]
 const PL011_BASE: usize = 0xFE20_1000;
+#[cfg(target_board = "versatilepb")]
+const PL011_BASE: usize = 0x101F_1000;
 
 register_bitfields! [
     u32,
